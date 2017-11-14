00272c978d26212266a3cdd2678fe70b377f1455
CrossedDS <ditra.d.singh@outlook.com>
2017-11-14 18:34:33 -0500

    Added jQuery and API info.

--- Beta/index.js
+++ Beta/index.js
@@ -1,15 +1,24 @@
-const NYT_TOP_API = "https://api.nytimes.com/svc/topstories/v2/home.json";
+const NYT_TOP_API = "https://api.nytimes.com/svc/topstories/v2/world.json";
 
-function getDataFromAPI() = {
+function getDataFromAPI(callback) = {
   let key = {
     'api-key': '7d043322ac9d4f7c9476f12845e76475',
   };
-  $.getJSON(NYT_TOP_API, key)
+  $.getJSON(NYT_TOP_API, key, callback)
 };
 
-//Renders news in Bootstrap Well
-$('#news-box').append(
-  `
+function renderResult(result) {
+  return `
+  <div>
+    <h2 id="result-header"><a href="${result.url}" target="_blank">${result.title}</a></h2>
+      <img id="result-image" src="${result.multimedia[2].url}">
+    </div>
+    `
+};
 
-  `
-)
+function displayNYTNews(data) {
+  const results = data.results.map((item, index) => renderResult(item));
+  $('#news-feed').html(results);
+};
+
+$(displayNYTNews);
